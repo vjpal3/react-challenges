17fcf0ce1458f3ca7b0f408e78b592e4a5f43fd6
vjpal3 <vrishalipal@yahoo.com>
2018-10-12 13:20:32 -0400

    add form validations

--- launch-academy-faq-part-2/src/containers/FAQFormContainer.js
+++ launch-academy-faq-part-2/src/containers/FAQFormContainer.js
@@ -7,23 +7,54 @@ class FAQFormContainer extends React.Component {
     this.state = {
       question: '',
       answer: '',
-      successMessage: ''
+      errors: {}
     }
 
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClearForm = this.handleClearForm.bind(this);
+    this.validateQuestion = this.validateQuestion.bind(this);
+    this.validateAnswer = this.validateAnswer.bind(this);
   }
 
   handleInputChange(event) {
+    if(event.target.name === 'question') {
+      this.validateQuestion(event.target.value)
+    } else {
+      this.validateAnswer(event.target.value)
+    }
     this.setState({[event.target.name]: event.target.value})
+  }
+
+  validateQuestion(input) {
+    if(input.trim() === "") {
+      let newError = { questionEntered: "Please enter a valid question" };
+      this.setState({errors: Object.assign({}, this.state.errors, newError)})
+      return false;
+    } else {
+      let errorState = this.state.errors;
+      delete errorState.questionEntered;
+      this.setState({ errors: errorState })
+      return true;
+    }
+  }
+
+  validateAnswer(input) {
+    if(input.trim() === "") {
+      let newError = { answerEntered: "Please enter a valid answer" };
+      this.setState({errors: Object.assign({}, this.state.errors, newError)})
+      return false;
+    } else {
+      let errorState = this.state.errors;
+      delete errorState.answerEntered;
+      this.setState({ errors: errorState })
+      return true;
+    }
   }
 
   handleFormSubmit(event) {
     event.preventDefault();
-    if(!this.state.question || !this.state.answer) {
-      this.setState({successMessage: "Please enter valid data."});
-    } else {
+    if((this.validateQuestion(this.state.question)) && (this.validateAnswer(this.state.answer))) {
       fetch('http://localhost:4567/api/v1/questions', {
         method: 'POST',
         headers: {
@@ -35,9 +66,7 @@ class FAQFormContainer extends React.Component {
         })
       }).then(res=>res.json())
         .then(res => {
-          this.setState({successMessage: 'Your data was submitted successfully!'});
           let formPayLoad = {question: this.state.question, answer: this.state.answer}
-
           this.props.addToFAQ(formPayLoad);
           //Important to put handleClearForm() here and not outside the fetch.
           //It will clear out the state before async call fetch finishes.
@@ -51,9 +80,18 @@ class FAQFormContainer extends React.Component {
   }
 
   render() {
+    let errorDiv, errorItems;
+    if(Object.keys(this.state.errors).length > 0){
+      errorItems = Object.values(this.state.errors).map((error) => {
+        return (
+          <li key={error}>{error}</li>
+        );
+      });
+      errorDiv = <div className="callout alert">{errorItems}</div>
+    }
     return (
       <div className='question-list'>
-        {this.state.successMessage}
+        {errorDiv}
         <FAQForm
           handleFormSubmit={this.handleFormSubmit}
           handleInputChange={this.handleInputChange}
