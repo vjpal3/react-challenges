47bd07978f235fc399f890fb89f7e28119a420ab
vjpal3 <vrishalipal@yahoo.com>
2018-10-12 14:41:20 -0400

    add tests for handler functions in form

--- launch-academy-faq-part-2/test/components/FAQFormSpec.js
+++ launch-academy-faq-part-2/test/components/FAQFormSpec.js
@@ -2,17 +2,23 @@ import FAQForm from '../../src/components/FAQForm';
 
 describe('FAQForm', () => {
   let wrapper;
+  let formSubmit = jasmine.createSpy('formSubmit spy');
+  let inputChange = jasmine.createSpy('inputChange spy');
+  let str='';
 
   beforeEach(() => {
     wrapper = mount(
-      <FAQForm />
+      <FAQForm
+        question={str}
+        handleFormSubmit={formSubmit}
+        handleInputChange={inputChange}
+      />
     );
   })
 
   it('renders a form with a input text', () => {
     expect(wrapper.find('input[type="text"]')).toBePresent();
     expect(wrapper.find('input[name="question"]')).toBePresent();
-    // expect(wrapper.find('input').prop('value')).toEqual('question');
   });
 
   it('renders a form with a textarea', () => {
@@ -25,4 +31,16 @@ describe('FAQForm', () => {
     let submitButton = wrapper.find('input[type="submit"]');
     expect(submitButton.prop('value')).toEqual('Submit FAQ');
   });
+
+  it('should invoke handleformSubmit function when submit button is clicked', () => {
+    wrapper.find('form').simulate('submit');
+    expect(formSubmit).toHaveBeenCalled();
+  })
+
+  it('should invoke handleInputChange function when form inputs are changed', () => {
+    wrapper.find('input[name="question"]').simulate('change', {target: {name: 'question', value: 'How about this new question?'}});
+    expect(inputChange).toHaveBeenCalled();
+    wrapper.find('textarea[name="answer"]').simulate('change', {target: {name: 'answer', value: 'This is new answer!'}});
+    expect(inputChange).toHaveBeenCalled();
+  })
 })
