2ddfe23584b6d6bd4162de30cfc37cdbb23757fb
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-30 17:24:42 -0500

    useState with an array

---
+++ React Hooks/useStateHook/RandomList.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+const RandomList = props => {
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.random() * 100
+      }
+    ]);
+  };
+
+  return (
+    <div>
+      {<button onClick={addItem}>Click to add one random number!</button>}
+      {items.length > 0 ? (
+        <span>
+          <p>The list contains following items: </p>
+          <ul>
+            {items.map(item => {
+              return <li key={item.id}>{item.value.toFixed(2)}</li>;
+            })}
+          </ul>
+        </span>
+      ) : (
+        <p>The list is empty! </p>
+      )}
+    </div>
+  );
+};
+
+render(<RandomList />, document.querySelector("#root"));
